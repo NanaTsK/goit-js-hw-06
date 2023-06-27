5f614704520537d95432c5927a3a6ff23000b70c
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-06-28 00:54:02 +0200

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+// element.querySelectorAll("#categories");
+
+// const listWithId = document.querySelectorAll('#categories');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log(listWithId);
+
