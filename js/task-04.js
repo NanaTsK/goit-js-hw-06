b266003656d9d52aade9af5c2d6f43705a90deef
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-06-29 12:45:00 +0200

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,19 @@
-// start
+let counterValue = 0;
+
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const counterEl = document.querySelector("#value");
+
+incrementBtn.addEventListener("click", incrementClick);
+
+function incrementClick() {
+    counterValue += 1;
+    counterEl.textContent = counterValue;
+};
+
+decrementBtn.addEventListener("click", decrementClick);
+
+function decrementClick() {
+    counterValue -= 1;
+    counterEl.textContent = counterValue;
+};
