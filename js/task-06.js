13fc70dac85757a4dfa88a4279d7766ad98c78ea
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-06-30 00:46:18 +0200

    task-6

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+const inputEl = document.querySelector("#validation-input");
+
+inputEl.addEventListener("blur", () => {
+    const inputLength = inputEl.value.length;
+    const inputSetLength = Number(inputEl.dataset.length);
+
+    if (inputLength === inputSetLength) {
+        inputEl.classList.remove("invalid");
+        inputEl.classList.add("valid");
+    } else if (inputLength === 0) {
+        inputEl.classList.remove("valid", "invalid");
+    } else {
+        inputEl.classList.add("invalid");
+        inputEl.classList.remove("valid")}
+
+    console.log(inputLength)   
+});
+
