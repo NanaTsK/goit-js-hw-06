25e90c23207f6431446299b704eb576d8d2b7448
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-01 18:13:54 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,38 @@
 
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", handleSubmit);
+
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    // let login = {
+    //     email: email.value,
+    //     password: password.value,
+    // }
+
+    const {elements: {email, password}} = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        alert(`Please fill out all the required fields`);
+    }
+    // console.log(login);
+    event.currentTarget.reset();
+};
+
+
+// const form = document.querySelector(".form");
+// form.addEventListener("submit", handleSubmit);
+
+
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const {elements: {login, password}
+//     } = event.currentTarget;
+//     if (login.value === "" || password.value === "") {
+//       return console.log("Please fill in all the fields!");
+//     } 
+//     console.log(`Login: ${login.value}, Password: ${password.value}`);
+//     event.currentTarget.reset();
+  
+//   };
