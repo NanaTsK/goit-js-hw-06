90f31dd5557d380d13fece3c71ef218b2c40c398
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-01 23:42:20 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -2,37 +2,19 @@
 const loginForm = document.querySelector(".login-form");
 loginForm.addEventListener("submit", handleSubmit);
 
-
-
 function handleSubmit(event) {
     event.preventDefault();
 
-    // let login = {
-    //     email: email.value,
-    //     password: password.value,
-    // }
-
     const {elements: {email, password}} = event.currentTarget;
     if (email.value === "" || password.value === "") {
-        alert(`Please fill out all the required fields`);
+      
+        return alert(`Please fill out all the fields`);
+        // return;
     }
-    // console.log(login);
+           const loginObj = {
+        email: email.value,
+        password: password.value,
+    }
+    console.log(loginObj);
     event.currentTarget.reset();
 };
-
-
-// const form = document.querySelector(".form");
-// form.addEventListener("submit", handleSubmit);
-
-
-// function handleSubmit(event) {
-//     event.preventDefault();
-//     const {elements: {login, password}
-//     } = event.currentTarget;
-//     if (login.value === "" || password.value === "") {
-//       return console.log("Please fill in all the fields!");
-//     } 
-//     console.log(`Login: ${login.value}, Password: ${password.value}`);
-//     event.currentTarget.reset();
-  
-//   };
