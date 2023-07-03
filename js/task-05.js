0d74ee6512110e4dd82e999f13c3c6e612c6354f
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-03 15:32:27 +0200

    task-5

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,9 @@ const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (textInput) => {
-    textInput.currentTarget.value === ""
-    ? textOutput.textContent = "Anonymous"
-    : textOutput.textContent = textInput.currentTarget.value
+    textInput.currentTarget.value.trim() === "" ? 
+    textOutput.textContent = "Anonymous"  
+    : textOutput.textContent = textInput.currentTarget.value.trim()
   });
 
   
