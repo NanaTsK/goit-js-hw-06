fbc0ddfd4fcdc873a27be4c7b1b793ae5e90faf3
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-05 14:01:10 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -9,38 +9,47 @@ const createBtnEl = document.querySelector("button[data-create]");
 const destroyBtnEl = document.querySelector("button[data-destroy]");
 const boxesEl = document.querySelector("#boxes");
 
+const min = Number(inputEl.getAttribute("min"));
+const max = Number(inputEl.getAttribute("max"));
+const step = Number(inputEl.getAttribute("step"));
+
 inputEl.addEventListener("input", onGetNumber);
 createBtnEl.addEventListener("click", onCreate);
 destroyBtnEl.addEventListener("click", clearBoxes);
 
 function onGetNumber(event) { 
-createBtnEl.value = event.currentTarget.value
-};
+  createBtnEl.value = event.currentTarget.value;
+}
 
 function onCreate() { 
-  createBoxes(createBtnEl.value) 
-};
+  const value = Number(createBtnEl.value);
+  if (value < min || value > max) {
+    alert(`Value must be a number between ${min} and ${max}`);
+    return;
+  }
+  createBoxes(value);
+}
 
 function createBoxes(amount) { 
   let boxSize = 30;
   const boxesArr = [];
 
   if (amount > 0) {
-    for (let i = 0; i < amount; i += 1) {
-    const boxEl = document.createElement("div");
+    for (let i = 0; i < amount; i += step) {
+      const boxEl = document.createElement("div");
 
-    boxEl.style.width = `${boxSize}px`;
-    boxEl.style.height = `${boxSize}px`;
-    boxEl.style.backgroundColor = getRandomHexColor(); 
-    boxSize += 10; 
-    boxesArr.push(boxEl);
+      boxEl.style.width = `${boxSize}px`;
+      boxEl.style.height = `${boxSize}px`;
+      boxEl.style.backgroundColor = getRandomHexColor(); 
+      boxSize += 10; 
+      boxesArr.push(boxEl);
     }
   }
-  boxesEl.append(...boxesArr)
-};
+  boxesEl.append(...boxesArr);
+}
 
 function clearBoxes() { 
   boxesEl.innerHTML = "";
   inputEl.value = "";
   createBtnEl.value = "";
-};
+}
